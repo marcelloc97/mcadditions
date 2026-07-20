022ff2d0508b1817d067accd78a702e1282389b5
Marcello Carvalhal <marcellocontact97@gmail.com>
2026-07-20 02:22:01 -0300

    fix: shears don't get consumed when crafting with wool or cobweb

--- src/main/java/net/mcdev/mcadditions/recipe/MCARecipes.java
+++ src/main/java/net/mcdev/mcadditions/recipe/MCARecipes.java
@@ -2,20 +2,18 @@ package net.mcdev.mcadditions.recipe;
 
 import net.mcdev.mcadditions.MCAdditions;
 import net.mcdev.mcadditions.recipe.custom.StringCobwebRecipe;
-//import net.mcdev.mcadditions.recipe.custom.StringWoolRecipe;
-import net.mcdev.mcadditions.recipe.custom.StringWoolRecipeHardcoded;
+import net.mcdev.mcadditions.recipe.custom.StringWoolRecipe;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class MCARecipes {
     // Serializers
-    public static final StringWoolRecipeHardcoded.Serializer STRING_WOOL_RECIPE_SERIALIZER =
+    public static final CustomRecipeSerializer<StringWoolRecipe> STRING_WOOL_RECIPE_SERIALIZER =
         Registry.register(
             Registries.RECIPE_SERIALIZER,
             new Identifier(MCAdditions.MOD_ID, "string_wool"),
-            new StringWoolRecipeHardcoded.Serializer()
-//            new CustomRecipeSerializer<>(StringWoolRecipe::new)
+            new CustomRecipeSerializer<>(StringWoolRecipe::new)
         );
 
     public static final CustomRecipeSerializer<StringCobwebRecipe> STRING_COBWEB_RECIPE_SERIALIZER =
